8fe515c543745b50a61aff53e36803ba11f520e1
José Daniel Gómez <joseg@manzanares.com.ve>
2021-10-14 15:48:41 -0400

    Initial commit for cocoapod lib template

---
+++ Example/Pods/Target Support Files/LukaSDK/LukaSDK-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LukaSDK : NSObject
+@end
+@implementation PodsDummy_LukaSDK
+@end
---
+++ Example/Pods/Target Support Files/Pods-LukaSDK_Example/Pods-LukaSDK_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LukaSDK_Example : NSObject
+@end
+@implementation PodsDummy_Pods_LukaSDK_Example
+@end
